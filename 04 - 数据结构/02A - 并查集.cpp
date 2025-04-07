e4b9faf32b486b9593d95632300837969e63e671
yhLang <1342237439@qq.com>
2025-04-07 12:47:16 +0800

    更新DSU,增加groups方法

--- 04 - 数据结构/02A - 并查集.cpp
+++ 04 - 数据结构/02A - 并查集.cpp
@@ -1,9 +1,10 @@
 struct DSU {
+    int n;
     vector<int> f, siz;
     
     DSU() {}
-    DSU(int n) {
-        init(n);
+    DSU(int _n) : n(_n) {
+        init(_n);
     }
     
     void init(int n) {
@@ -37,6 +38,25 @@ struct DSU {
     int size(int x) {
         return siz[find(x)];
     }
+
+    auto groups() {
+        vector<int> find_buf(n), grp_siz(n);
+        for (int i = 0; i < n; i++) {
+            find_buf[i] = find(i);
+            grp_siz[find_buf[i]] += 1;
+        }
+        vector res(n, vector<int>());
+        for (int i = 0; i < n; i++) {
+            res[i].reserve(grp_siz[i]);
+        }
+        for (int i = 0; i < n; i++) {
+            res[find_buf[i]].push_back(i);
+        }
+        res.erase(
+            remove_if(begin(res), end(res),
+                           [&](const vector<int>& v) { return empty(v); }),
+            end(res));
+        return res;
+    }
+
 };
-
-
