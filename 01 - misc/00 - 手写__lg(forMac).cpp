ac6c6b38c12a860c6d6b562c99dffcc785b0cf28
yhLangMac <1342237439@qq.com>
2025-04-10 17:40:41 +0800

    增加 cosntexpr 标识

--- 01 - misc/00 - 手写__lg(forMac).cpp
+++ 01 - misc/00 - 手写__lg(forMac).cpp
@@ -1,3 +1,3 @@
 #ifdef LOCAL
-inline int __lg(int __n) { return sizeof(int) * __CHAR_BIT__  - 1 - __builtin_clz(__n); }
+inline constexpr int __lg(int __n) { return sizeof(int) * __CHAR_BIT__  - 1 - __builtin_clz(__n); }
 #endif
