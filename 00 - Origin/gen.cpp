13eca8492698eeb831f5a959ce9e5fd7aecb8932
yhLang <1342237439@qq.com>
2025-04-10 23:44:16 +0800

    更新solve函数为lambda

--- 00 - Origin/gen.cpp
+++ 00 - Origin/gen.cpp
@@ -1,60 +1,70 @@
 #include <bits/stdc++.h>
- 
+
 using namespace std;
- 
-template<class F, class S>
-ostream &operator<<(ostream &s, const pair<F, S> &v) {
+
+template <class F, class S>
+ostream& operator<<(ostream& s, const pair<F, S>& v)
+{
     s << "(" << v.first << ", " << v.second << ")";
     return s;
 }
-template<ranges::range T> requires (!is_convertible_v<T, string_view>)
-istream &operator>>(istream &s, T &&v) { 
-    for (auto &&x : v) s >> x; 
-    return s; 
+template <ranges::range T>
+    requires(!is_convertible_v<T, string_view>)
+istream& operator>>(istream& s, T&& v)
+{
+    for (auto&& x : v)
+        s >> x;
+    return s;
 }
-template<ranges::range T> requires (!is_convertible_v<T, string_view>)
-ostream &operator<<(ostream &s, T &&v) { 
-    for (auto &&x : v) s << x << ' '; 
-    return s; 
+template <ranges::range T>
+    requires(!is_convertible_v<T, string_view>)
+ostream& operator<<(ostream& s, T&& v)
+{
+    for (auto&& x : v)
+        s << x << ' ';
+    return s;
 }
- 
+
 #ifdef LOCAL
-template<class... T> void dbg(T... x) {
-    char e{};
+template <class... T>
+void dbg(T... x)
+{
+    char e {};
     ((cerr << e << x, e = ' '), ...);
 }
 #define debug(x...) dbg(#x, '=', x, '\n')
 #else
 #define debug(...) ((void)0)
 #endif
- 
-template<class T> inline constexpr T INF = numeric_limits<T>::max() / 2;
-template<class T> bool chmin(T &a, T b) { return (b < a) and (a = b, true); }
-template<class T> bool chmax(T &a, T b) { return (a < b) and (a = b, true); }
+
+template <class T>
+inline constexpr T INF = numeric_limits<T>::max() / 2;
+template <class T>
+bool chmin(T& a, T b) { return (b < a) and (a = b, true); }
+template <class T>
+bool chmax(T& a, T b) { return (a < b) and (a = b, true); }
 
 using u32 = uint32_t;
 using i64 = int64_t;
 using u64 = uint64_t;
 using i128 = __int128_t;
 
-void solve() 
+signed main()
 {
-    srand(time(0));
-
-}
-
-signed main() {
     cin.tie(nullptr)->sync_with_stdio(false);
     cin.exceptions(cin.failbit);
-    
+
+    auto solve = [&]() {
+        srand(time(0));
+        
+    };
+
     int t{1};
-#ifdef tests
-    cin >> t;
-#endif
+    // cin >> t;
 
     while (t--) {
         solve();
     }
-    
+
     return 0;
 }
--- 00 - Origin/pat.cpp
+++ 00 - Origin/pat.cpp
@@ -1,36 +1,35 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-template<class F, class S>
+template <class F, class S>
 ostream& operator<<(ostream& s, const pair<F, S>& v)
 {
-    return s << "(" << v.first << ", " << v.second << ")";
+    s << "(" << v.first << ", " << v.second << ")";
+    return s;
 }
-
 template <ranges::range T>
     requires(!is_convertible_v<T, string_view>)
 istream& operator>>(istream& s, T&& v)
 {
-    for (auto&& x : v) 
+    for (auto&& x : v)
         s >> x;
     return s;
 }
-
-template<ranges::range T>
+template <ranges::range T>
     requires(!is_convertible_v<T, string_view>)
 ostream& operator<<(ostream& s, T&& v)
 {
-    for (auto&& x : v) 
+    for (auto&& x : v)
         s << x << ' ';
     return s;
 }
 
 #ifdef LOCAL
-template<class... T>
-void dbg(T... x) 
+template <class... T>
+void dbg(T... x)
 {
-    char e{};
+    char e {};
     ((cerr << e << x, e = ' '), ...);
 }
 #define debug(x...) dbg(#x, '=', x, '\n')
@@ -38,48 +37,46 @@ void dbg(T... x)
 #define debug(...) ((void)0)
 #endif
 
-template<class T>
-inline constexpr T inf = numeric_limits<T>::max() / 2;
-template<class T>
-bool chmin(T& a, T b) {return (b < a) and (a = b, true);}
-template<class T>
-bool chmax(T& a, T b) {return (a < b) and (a = b, true);}
+template <class T>
+inline constexpr T INF = numeric_limits<T>::max() / 2;
+template <class T>
+bool chmin(T& a, T b) { return (b < a) and (a = b, true); }
+template <class T>
+bool chmax(T& a, T b) { return (a < b) and (a = b, true); }
 
 using u32 = uint32_t;
 using i64 = int64_t;
 using u64 = uint64_t;
 using i128 = __int128_t;
 
-auto solve()
-{
-    const int N{100'000};
-    system("g++ -std=c++20 brt.cpp -o brt");//bruteforce
-    system("g++ -std=c++20 sol.cpp -o sol");//solution
-    system("g++ -std=c++20 gen.cpp -o gen");//data generator
-    for (int t = 1; t < N; t++) {
-        system("./gen > data.in");
-        system("./brt < data.in > brt.out");
-        system("./sol < data.in > sol.out");
-        if (system("diff brt.out sol.out > diff.log")) {
-            cout << "WRONG" << endl; break ;
-        }
-        cout << "PASS" << endl;
-    }
-}
-
 signed main()
 {
     cin.tie(nullptr)->sync_with_stdio(false);
     cin.exceptions(cin.failbit);
 
-    int t{1};
-#ifdef tests
+    auto solve = [&]() {
+        const int N { 100'000 };
+        system("g++ -std=c++20 brt.cpp -o brt"); // bruteforce
+        system("g++ -std=c++20 sol.cpp -o sol"); // solution
+        system("g++ -std=c++20 gen.cpp -o gen"); // data generator
+        for (int t = 1; t < N; t++) {
+            system("./gen > data.in");
+            system("./brt < data.in > brt.out");
+            system("./sol < data.in > sol.out");
+            if (system("diff brt.out sol.out > diff.log")) {
+                cout << "WRONG" << endl;
+                break;
+            }
+            cout << "PASS" << endl;
+        }
+    };
+
+    int t { 1 };
     cin >> t;
-#endif
+
     while (t--) {
         solve();
     }
 
     return 0;
-
 }
