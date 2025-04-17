43fa432751ff08283aa19a7f3f0482386af1a453
yhLang <1342237439@qq.com>
2025-04-17 10:36:39 +0800

    更新文件逻辑，最小费用可行流和最小费用最大流整合到费用流

--- 02 - 图、网络流、树/07B - 费用流.cpp
+++ 02 - 图、网络流、树/07B - 费用流.cpp
@@ -1,3 +1,4 @@
+// 最小费用最大流
 template <class T, class F>
 class MCFGraph {
     // 定义边的结构体，保存目标点、剩余容量和费用
@@ -90,3 +91,21 @@ public:
     }
 };
 
+// 最小费用可行流
+template <class T, class F>
+class MCFGraphOK {
+    MCFGraph<T, F> flow;
+    int superStr;
+    T totCap;
+public:
+    MCFGraphOK(int n) : flow(n + 1), superStr{n}, totCap{} {}
+    void add(int x, int y, T cap, F fees) {
+        flow.add(x, y, cap, fees);
+        totCap += cap;
+    }
+    pair<T, F> work(int s, int t) {
+        flow.add(superStr, s, totCap, 0);
+        flow.add(s, t, totCap, 0);
+        return flow.work(superStr, t);
+    }
+};
